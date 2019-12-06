60cbf2a0844087d2118d864e06a000ccd82f3086
araaaaar <rira9250@gmail.com>
2019-12-06 16:07:19 +0900

    テーブル作成、初期データ投入、コントローラ作成

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        // DB::table('questions')->truncate();
+        // DB::table('questions')->insert([
+        //     [
+        //         'user_id' => 4,
+        //         'question_id' => 4,
+        //         'comment' => 'comment test',
+        //     ],
+        // ]);
+    }
+}
---
+++ database/seeds/QuestionsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class QuestionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('questions')->truncate();
+        DB::table('questions')->insert([
+            [
+                'user_id' => 4,
+                'tag_category_id' => 1,
+                'title' => 'test1',
+                'content' => 'test1',
+            ],
+            [
+                'user_id' => 4,
+                'tag_category_id' => 1,
+                'title' => 'test2',
+                'content' => 'test2',
+            ],
+        ]);
+    }
+}
